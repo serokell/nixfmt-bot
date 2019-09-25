5d7b373e5f98a6aea513bce199f067e1f82ef4fd
Ilya Peresadin <pva701@gmail.com>
2019-09-25 19:57:49 +0300

    Filter only related payload of IssueCommentEvent #1

---
+++ src/Nixfmt/GitHubEvent.hs
@@ -0,0 +1,25 @@
+module Nixfmt.GitHubEvent
+       ( isIssueEvent
+       , isPullRequestEvent
+       , isBotMention
+       , isCreatedEvent
+       ) where
+
+import qualified Data.Text as T
+import GitHub.Data.Webhooks.Events  (IssueCommentEvent (..), IssueCommentEventAction (..))
+import GitHub.Data.Webhooks.Payload (getUrl, HookIssue (..))
+
+isIssueEvent :: IssueCommentEvent -> Bool
+isIssueEvent = endsWith "issues" . getUrl . whIssueHtmlUrl . evIssueCommentIssue
+
+isPullRequestEvent :: IssueCommentEvent -> Bool
+isPullRequestEvent = endsWith "pull" . getUrl . whIssueHtmlUrl . evIssueCommentIssue
+
+isBotMention :: Text -> Bool
+isBotMention = ("@nixfmt-bot" == )
+
+isCreatedEvent :: IssueCommentEvent -> Bool
+isCreatedEvent ev = evIssueCommentAction ev == IssueCommentCreatedAction
+
+endsWith :: Text -> Text -> Bool
+endsWith sub s = sub `T.isSuffixOf` T.init (T.dropWhileEnd (/= '/') s)
--- src/Nixfmt/WebhookHandlers.hs
+++ src/Nixfmt/WebhookHandlers.hs
@@ -3,12 +3,14 @@ module Nixfmt.WebhookHandlers
        ) where
 
 import GitHub.Data.Webhooks.Events  (IssueCommentEvent (..))
+import GitHub.Data.Webhooks.Payload  (HookIssueComment (..))
 import Servant.GitHub.Webhook (RepoWebhookEvent (..))
 import Servant.Server (Handler)
 import Servant.API.Generic (ToServant)
 import Servant.Server.Generic (AsServer, genericServer)
 
 import Nixfmt.ServerAPI (NixfmtWebhooks (..))
+import Nixfmt.GitHubEvent
 
 type NixfmtHandlers = ToServant NixfmtWebhooks AsServer
 
@@ -18,4 +20,9 @@ nixfmtHandlers = genericServer NixfmtWebhooks
     }
 
 onIssueCommentEvent :: RepoWebhookEvent -> ((), IssueCommentEvent) -> Handler ()
-onIssueCommentEvent _ (_, event) = putTextLn $ "Event: " <> show event
+onIssueCommentEvent _ (_, event@IssueCommentEvent{..})
+    | isPullRequestEvent event
+    && isBotMention (whIssueCommentBody evIssueCommentPayload)
+    && isCreatedEvent event =
+        putTextLn $ "Issue comment text: " <> whIssueCommentBody evIssueCommentPayload
+    | otherwise  = putTextLn "Event has been skipped"
